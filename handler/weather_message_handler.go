7ca2f6bdeb25b8b66601a8f2cc65492124dcb018
Arnaud Tarroux <ta.arnaud@gmail.com>
2019-11-19 23:18:52 +0100

    persist weather event only each 15 minutes

--- handler/weather_message_handler.go
+++ handler/weather_message_handler.go
@@ -17,12 +17,21 @@ func (h WeatherMessageHandler) supports(message message) bool {
 	return message.MessageType == "weather"
 }
 
+var lastRegisteredWeatherEvent time.Time
+
 func (h WeatherMessageHandler) execute(message message) {
 	log.Printf("Executing... ControllerID: %s | MessageType: %s\n", message.ControllerID, message.MessageType)
 
+	now := time.Now()
+	// We persist the events in database only each 15 minutes
+	if !now.After(lastRegisteredWeatherEvent.Add(time.Minute * 15)) {
+		return
+	}
+
+	lastRegisteredWeatherEvent = now
 	uuid, _ := uuid.NewUUID()
 	payload := string(message.Payload)
-	event := model.NewEvent(uuid.String(), message.ControllerID, message.MessageType, payload, time.Now())
+	event := model.NewEvent(uuid.String(), message.ControllerID, message.MessageType, payload, now)
 
 	eventRepository := new(repositories.SqlEventRepository)
 	eventRepository.Save(event)
